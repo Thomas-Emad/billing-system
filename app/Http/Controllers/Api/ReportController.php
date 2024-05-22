45a88519393d18bdca3b7b01118d7e61b78e8b59
89dfc4bca9 <160602868+89dfc4bca9@users.noreply.github.com>
2024-05-22 08:55:04 +0300

    Test Reporst

---
+++ app/Http/Controllers/Api/ReportController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Product;
+use App\Models\Invoice;
+
+class ReportController extends Controller
+{
+    public function products() {
+
+        $count_products = Count(Product::all());
+        if($count_products > 0) {
+
+            return response()->json([
+                'Count Products' => $count_products
+            ], 200);
+
+        } else {
+
+            return response()->json([
+                'message' => "No Product found"
+            ], 200);
+
+        }
+
+    }
+
+    public function Invoices() {
+
+        $count_invoices = Count(Invoice::all());
+        if($count_invoices > 0) {
+
+            return response()->json([
+                'Count Invoices' => $count_invoices
+            ], 200);
+
+        } else {
+
+            return response()->json([
+                'message' => "No Invoices found"
+            ], 200);
+
+        }
+
+    }
+
+    public function InvoicesUnPaid() {
+
+        $count_invoices_Unpaid = Count(Invoice::where('status', 'unpaid')->get());
+        if($count_invoices_Unpaid > 0) {
+
+            return response()->json([
+                'Count Invoices Unpaid' => $count_invoices_Unpaid
+            ], 200);
+
+        } else {
+
+            return response()->json([
+                'message' => "No Invoices Unpaid found"
+            ], 200);
+
+        }
+
+    }
+
+    public function profits() {
+
+        $invoices = Invoice::all();
+        $profits = $invoices->sum('profits');
+        return response()->json([
+            'profits' => $profits
+        ], 200);
+    }
+
+}
